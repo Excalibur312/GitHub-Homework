38e54b000ba000f51b05ee31a3e23824702760b1
Emre Kaya <133666788+Excalibur312@users.noreply.github.com>
2024-03-13 15:16:57 +0300

    Add files via upload

---
+++ untitled.m
@@ -0,0 +1,85 @@
+% %  prompt = "What is the value? ";
+% %  number= input(prompt);
+% %  result1= power(number,2);
+% %  result2=number^2;
+% %  result3= sqrt(number);
+% %  disp('Result 1 = ' );
+% %  disp(result1);
+% %  disp('Result 2 = '); 
+% %  disp(result2);
+% %  disp('Result 3 = '); 
+% %  disp(result3);
+% %  prompt = "Enter a message to have lenght ";
+% %  word= input(prompt);
+% %  lengcharacter=length(word);
+% %  disp(lengcharacter);
+% %  x=sin(pi/2);
+% %  disp(x);
+% % float (12456.36);
+% % x=12456.36*6;
+% % disp(x);
+% % number1= input("Please the enter number");
+% % number2 = input ("please the enter number ");
+% % total= number1 + number2;
+% % disp(total);
+% % initial_value =25;
+% % xpected_value 25 percent of the inital value
+% % d=25;
+% % number= d*3/4;
+% % display(number);
+% % calculateMappingAvailable
+% % format short e
+% % x= 10+25+360^36+3652354;
+% % disp(x);
+% % A= [1, 2, 3 ; 4, 5, 6;7, 8, 9];
+% % B= [9, 8, 7 ; 6, 5, 4, ; 3 , 2, 1];
+% % disp(A);
+% % disp(B);
+% % C=A+B;
+% % disp(C);
+% % D=A*B;
+% % disp(D);
+% % E=A./B;
+% % disp(E);
+% % transposeA= A';
+% % disp(transposeA);
+% % transposeB= B';
+% % disp(transposeB);
+% % transposeC= C';
+% % disp(transposeC);
+% 
+% n_student=5600;
+% n_teachingstaff=40;
+% n_personnel =140;
+% 
+% n_total=n_personnel+n_teachingstaff+n_student;
+% d=det(n_total);
+% tranposeTotal=n_total';
+% sinteach= sin(n_teachingstaff);
+% expstaff = exp(n_personnel);
+% disp(n_total);
+% disp(tranposeTotal)
+% disp(d)
+% disp(expstaff);
+% disp(sinteach);
+% 
+% 
+% A= [1 2 3 4 5 6 7];
+% B= [9 8 7 6 5 4 3];
+% summary= A + B ;
+% divided= A./B;
+% intersect_AB=intersect(A,B);
+% ismember_AB=ismember(A,B);
+% union_AB= union(A,B);
+% disp(summary);
+% disp(divided);
+% disp(intersect_AB);
+% disp(ismember_AB);
+% disp(union_AB);
+% C= zeros(50);
+% D=eye(50);
+% E=ones(50,40);
+% disp(C);
+% disp(D);
+% disp(E);
+% 
